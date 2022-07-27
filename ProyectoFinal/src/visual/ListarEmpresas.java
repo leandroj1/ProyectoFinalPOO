40a11e29c76439a91310029c280a030edcc54777
Leandro Jiménez <LXJP0001@ce.pucmm.edu.do>
2022-07-27 16:51:53 -0400

    interfaz de listar empresas

---
+++ ProyectoFinal/src/visual/ListarEmpresas.java
@@ -0,0 +1,193 @@
+package visual;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+
+import customs.NonEditableTable;
+import logico.*;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class ListarEmpresas extends JDialog {
+	private final JPanel contentPanel = new JPanel();
+	private JTable tablaEmpresas;
+	private DefaultTableModel model;
+	private Object[] row;
+	private JButton btnVerSolicitudes;
+	private Empresa selectedEmpresa = null;
+	private JButton btnVerSolicitudesPendientes;
+	private JTextField txtRNC;
+	private JButton btnVerDetalles;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ListarEmpresas dialog = new ListarEmpresas();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ListarEmpresas() {
+		setTitle("Listado de empresas");
+		// TODO: agregar tipo de empresa
+		final String[] headers = {
+				"RNC",
+				"Nombre comercial",
+				"Razón social",
+				"Sector",
+				"Rubro",
+				"Ubicación"
+		};
+
+		setResizable(false);
+		setBounds(100, 100, 921, 570);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
+		contentPanel.setBounds(0, 0, 904, 494);
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel);
+		contentPanel.setLayout(new BorderLayout(0, 0)); {
+			JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			contentPanel.add(panel, BorderLayout.CENTER);
+			panel.setLayout(null);
+			{
+				JScrollPane scrollPane = new JScrollPane();
+				scrollPane.setBounds(8, 78, 879, 395);
+				scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+				panel.add(scrollPane); {
+					tablaEmpresas = new NonEditableTable();
+					tablaEmpresas.getTableHeader().setReorderingAllowed(false);
+					tablaEmpresas.addMouseListener(new MouseAdapter() {
+						public void mouseClicked(MouseEvent e) {
+							int index = tablaEmpresas.getSelectedRow();
+							if(index >= 0) {
+								String codigoString = tablaEmpresas.getValueAt(index, 0).toString();
+								ArrayList<Empresa> result = BolsaTrabajo.getInstance().getEmpresasByID(codigoString);
+								if(result.size() != 0) {
+									selectedEmpresa = result.get(0);									
+									if(selectedEmpresa != null) {
+										setButtonsState(true);
+									}
+								}
+							}
+						}
+					});
+					model = new DefaultTableModel();
+					model.setColumnIdentifiers(headers);
+					tablaEmpresas.setModel(model);
+					scrollPane.setViewportView(tablaEmpresas);
+				}
+			}
+
+			JPanel panelFilter = new JPanel();
+			panelFilter.setBorder(new TitledBorder(null, "Filtrado", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panelFilter.setBounds(8, 11, 879, 56);
+			panel.add(panelFilter);
+			panelFilter.setLayout(null);
+
+			JLabel lblNewLabel = new JLabel("RNC:");
+			lblNewLabel.setBounds(10, 22, 46, 14);
+			panelFilter.add(lblNewLabel);
+
+			txtRNC = new JFormattedTextField(Utils.getMaskCedula());
+			txtRNC.setBounds(43, 19, 457, 20);
+			panelFilter.add(txtRNC);
+			txtRNC.setColumns(10);
+
+			JButton btnFilter = new JButton("Filtrar datos");
+			btnFilter.setBounds(510, 17, 138, 23);
+			panelFilter.add(btnFilter);
+
+			JButton btnReset = new JButton("Mostrar todas las empresas");
+			btnReset.setEnabled(false);
+			btnReset.setBounds(658, 17, 211, 23);
+			panelFilter.add(btnReset);
+		} 
+		JPanel buttonPane = new JPanel();
+		buttonPane.setBounds(0, 494, 904, 33);
+		getContentPane().add(buttonPane); {
+			btnVerSolicitudes = new JButton("Ver solicitudes de trabajadores");
+			btnVerSolicitudes.setBounds(553, 5, 224, 23);
+			btnVerSolicitudes.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if(selectedEmpresa != null) {
+						// Abrir ventana
+
+						// Para evitar errores
+						setButtonsState(false);
+					}
+				}
+			});
+			buttonPane.setLayout(null);
+			btnVerSolicitudes.setEnabled(false);
+			buttonPane.add(btnVerSolicitudes);
+		}{
+			JButton btnCancelar = new JButton("Cancelar");
+			btnCancelar.setBounds(787, 5, 112, 23);
+			btnCancelar.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+				}
+			});
+
+			btnCancelar.setActionCommand("Cancel");
+			buttonPane.add(btnCancelar);
+		}
+		btnVerSolicitudesPendientes = new JButton("Ver solicitudes pendientes");
+		btnVerSolicitudesPendientes.setEnabled(false);
+		btnVerSolicitudesPendientes.setBounds(357, 5, 186, 23);
+		buttonPane.add(btnVerSolicitudesPendientes);
+		
+		btnVerDetalles = new JButton("Ver detalles");
+		btnVerDetalles.setEnabled(false);
+		btnVerDetalles.setBounds(194, 5, 143, 23);
+		buttonPane.add(btnVerDetalles);
+
+		loadRowsInTable(BolsaTrabajo.getInstance().getEmpresasByID(""));
+	}
+
+	// Cargar datos a la tabla
+	private void loadRowsInTable(ArrayList<Empresa> empresas) {
+		row = new Object[model.getColumnCount()];
+		for (Empresa empresa : empresas) {
+			row[0] = empresa.getRNC();
+			row[1] = empresa.getNombreComercial();
+			row[2] = empresa.getRazonSocial();
+			row[3] = empresa.getSector();
+			row[4] = empresa.getRubro();
+			row[5] = empresa.getUbicacion().toString();
+
+			model.addRow(row);
+		}
+	}
+
+	// Cambiar el estado de los botones de solicitudes
+	private void setButtonsState(boolean isEnabled) {
+		btnVerSolicitudes.setEnabled(isEnabled);
+		btnVerSolicitudesPendientes.setEnabled(isEnabled);
+		btnVerDetalles.setEnabled(isEnabled);
+	}
+}
