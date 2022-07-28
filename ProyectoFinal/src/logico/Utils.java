367ee929144ac728c8d4471a63e6f4643e06ccb1
Leandro Jiménez <LXJP0001@ce.pucmm.edu.do>
2022-07-28 12:04:15 -0400

    Nuevas utils para validar emails y generar una mask de formateo

--- ProyectoFinal/src/logico/Utils.java
+++ ProyectoFinal/src/logico/Utils.java
@@ -4,6 +4,7 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
@@ -99,7 +100,7 @@ public class Utils {
 		}
 	}
 
-	// Para saber si un combobox está en el valor por defecto ("<Seleccione>")
+	// Para saber si un combobox estï¿½ en el valor por defecto ("<Seleccione>")
 	public static boolean isCbxDefaultValue(JComboBox comboBox) {
 		return comboBox.getSelectedIndex() <= 0;
 	}
@@ -114,5 +115,32 @@ public class Utils {
 			}
 		}
 		return null;
+	}
+
+	// Obtener una mascara para los id's
+	public static MaskFormatter getNumberMaskByPrefix(String prefix, int lenght) {
+		MaskFormatter mask = null;
+		try {
+			StringBuffer buffer = new StringBuffer();
+			for (int i = 0; i < lenght; i++) {
+				buffer.append("#");
+			}
+
+			mask = new MaskFormatter(prefix + buffer.toString());
+			mask.setPlaceholderCharacter(defaultPlaceholder);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return mask;
+	}
+
+	// Regex para evalaur si un email es valido
+	public static boolean isAValidEmail(String email) {
+		final String regex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" 
+        + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+		final Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+		return pattern.matcher(email).find();
 	}
 }
