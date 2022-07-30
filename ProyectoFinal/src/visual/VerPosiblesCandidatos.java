8baf5c5299dbdea62e7c3b7f856c05b16f75b5a5
Leandro Jiménez <LXJP0001@ce.pucmm.edu.do>
2022-07-30 11:42:09 -0400

    Condiciones de contratacion

--- ProyectoFinal/src/visual/VerPosiblesCandidatos.java
+++ ProyectoFinal/src/visual/VerPosiblesCandidatos.java
@@ -5,17 +5,20 @@ import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.table.DefaultTableModel;
 
 import customs.CheckBoxsEditableTable;
-import customs.NonEditableTable;
 import logico.SolicitudEmpresa;
 
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 
 public class VerPosiblesCandidatos extends JDialog {
@@ -26,12 +29,14 @@ public class VerPosiblesCandidatos extends JDialog {
 	private SolicitudEmpresa solicitudLoaded = null;
 	private JButton btnContratar;
 
+	private int kColumnaCheckboxes = 0;
+
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		try {
-			VerPosiblesCandidatos dialog = new VerPosiblesCandidatos(null);
+			VerPosiblesCandidatos dialog = new VerPosiblesCandidatos(new SolicitudEmpresa(null, null, 1, 2, 2, 2, 1, null, null, null, false, false, null, false, null, null, null, 70));
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -44,11 +49,9 @@ public class VerPosiblesCandidatos extends JDialog {
 	 */
 	public VerPosiblesCandidatos(SolicitudEmpresa solicitud) {
 		solicitudLoaded = solicitud;
-
-		final Object[] columnNames = {"Selección", "Nombre del candidato", "Porcentaje de March"};
-        Object[][] data = {};
-        DefaultTableModel model = new DefaultTableModel(data, columnNames);
-
+		final Object[] headers = {"Selección", "Nombre del candidato", "Porcentaje de March"};
+		this.model = new DefaultTableModel();
+		model.setColumnIdentifiers(headers);
 
 		setBounds(100, 100, 615, 433);
 		setLocationRelativeTo(null);
@@ -60,8 +63,8 @@ public class VerPosiblesCandidatos extends JDialog {
 			JScrollPane scrollPane = new JScrollPane();
 			contentPanel.add(scrollPane);
 			{
-				table = new CheckBoxsEditableTable(model, 0);
-		        table.setPreferredScrollableViewportSize(table.getPreferredSize());
+				table = new CheckBoxsEditableTable(this.model, kColumnaCheckboxes);
+				table.setPreferredScrollableViewportSize(table.getPreferredSize());
 				scrollPane.setViewportView(table);
 			}
 			{
@@ -70,8 +73,27 @@ public class VerPosiblesCandidatos extends JDialog {
 				getContentPane().add(buttonPane, BorderLayout.SOUTH);
 				{
 					btnContratar = new JButton("Contratar candidatos seleccionados");
+					btnContratar.setEnabled(true);
 					btnContratar.addActionListener(new ActionListener() {
 						public void actionPerformed(ActionEvent e) {
+							ArrayList<Integer> indicesFilasSeleccionadas = getIndexesFilasSeleccionadas();
+							int cantidadPlazasSeleccionadas = indicesFilasSeleccionadas.size();
+							if(cantidadPlazasSeleccionadas == 0) {
+								JOptionPane.showMessageDialog(null,
+										"Seleccione al menos un candidato.",
+										"Error",
+										JOptionPane.ERROR_MESSAGE);
+							}
+							else if(cantidadPlazasSeleccionadas > solicitudLoaded.getCantidadPlazasNecesarias()) {
+								int option = JOptionPane.showConfirmDialog(null, "La cantidad de plazas seleccionadas sobrepasa la acordada en la solicitud. ¿Desea actualizar la cantidad de plazas a " + cantidadPlazasSeleccionadas + "?\nSi no actualiza, tendr\u00e1 que seleccionar una cantidad de plazas igual a " + solicitud.getCantidadPlazasNecesarias()+".", "Confirmación | Cantidad de plazas", JOptionPane.YES_NO_OPTION);
+								if(JOptionPane.YES_OPTION == option) {
+									solicitudLoaded.setCantidadPlazasNecesarias(cantidadPlazasSeleccionadas);
+									contratar();
+								}
+							}
+							else {
+								contratar();
+							}
 						}
 					});
 					btnContratar.setActionCommand("OK");
@@ -90,4 +112,21 @@ public class VerPosiblesCandidatos extends JDialog {
 				}
 			}
 		}
-	}}
+	}
+
+	private ArrayList<Integer> getIndexesFilasSeleccionadas() {
+		ArrayList<Integer> indexes = new ArrayList<Integer>();
+
+		for (int rowIndex = 0; rowIndex < model.getRowCount(); rowIndex++) {
+			if((boolean)(this.model.getValueAt(rowIndex, kColumnaCheckboxes))) {
+				indexes.add(Integer.valueOf(rowIndex));
+			}
+		}
+
+		return indexes;
+	}
+
+	private void contratar() {
+		return;
+	}
+}
