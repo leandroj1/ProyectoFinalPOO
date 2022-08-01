c18771beb7fea51a92eb1780677abe1034546062
Jean Luis Jimenez <luis.jimenez@wmcontractor.com>
2022-08-01 12:03:22 -0400

    Adding validation to not add more than 3 active SolPersonal

--- ProyectoFinal/src/logico/BolsaTrabajo.java
+++ ProyectoFinal/src/logico/BolsaTrabajo.java
@@ -58,7 +58,7 @@ public class BolsaTrabajo {
 	public void agregarSolicitudEmpleado(String cedula, SolicitudPersonal solicitud) {
 		ArrayList<Personal> personalAux = getPersonalByID(cedula);
 
-		if (personalAux.size() == 1) {
+		if (personalAux.size() == 1 && getActiveSolPersonalByCedula(cedula).size() < 3) {
 			personalAux.get(0).agregarSolicitud(solicitud);
 			solicitudesPersonal.add(solicitud);
 		}
@@ -105,6 +105,15 @@ public class BolsaTrabajo {
 				solicitudPersonal.setEstado(EstadoSolicitudPersonal.ACTIVA);
 			}
 		});
+	}
+	
+	public ArrayList<SolicitudPersonal> getActiveSolPersonalByCedula(String cedula) {
+		ArrayList<SolicitudPersonal> solPersonalList = getSolicitudesPersonalByID(cedula, "");
+		ArrayList<SolicitudPersonal> solPersonalActive = new ArrayList<SolicitudPersonal>();
+		for (SolicitudPersonal solPersonal : solPersonalList)
+			if (solPersonal.getEstado() == EstadoSolicitudPersonal.ACTIVA)
+				solPersonalActive.add(solPersonal);
+		return solPersonalActive;
 	}
 
 	public void contratarPersonal(String cedula, String RNCEmpresaContratacion, String idSolicitudPersonal) {
